76fd5cd6c2768c0c42ec8a3786d0ab0d3b92756d
montag451 <montag451@laposte.net>
2015-12-04 18:30:41 +0100

    Initial Commit

---
+++ custom.el
@@ -0,0 +1,67 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(c-basic-offset 4)
+ '(column-number-mode t)
+ '(company-backends
+   (quote
+    (company-bbdb company-nxml company-css company-eclim company-clang company-semantic company-xcode company-cmake company-anaconda company-capf
+                  (company-dabbrev-code company-gtags company-etags company-keywords)
+                  company-oddmuse company-files company-dabbrev)))
+ '(company-clang-arguments (quote ("-std=c++11")))
+ '(company-quickhelp-mode t)
+ '(custom-enabled-themes (quote (tangotango)))
+ '(custom-safe-themes
+   (quote
+    ("5999e12c8070b9090a2a1bbcd02ec28906e150bb2cdce5ace4f965c76cf30476" default)))
+ '(desktop-save-mode t)
+ '(ediff-window-setup-function (quote ediff-setup-windows-plain))
+ '(evil-emacs-state-modes
+   (quote
+    (archive-mode bbdb-mode bookmark-bmenu-mode bookmark-edit-annotation-mode browse-kill-ring-mode bzr-annotate-mode calc-mode cfw:calendar-mode completion-list-mode Custom-mode debugger-mode delicious-search-mode desktop-menu-blist-mode desktop-menu-mode doc-view-mode dvc-bookmarks-mode dvc-diff-mode dvc-info-buffer-mode dvc-log-buffer-mode dvc-revlist-mode dvc-revlog-mode dvc-status-mode dvc-tips-mode ediff-mode ediff-meta-mode efs-mode Electric-buffer-menu-mode emms-browser-mode emms-mark-mode emms-metaplaylist-mode emms-playlist-mode etags-select-mode fj-mode gc-issues-mode gdb-breakpoints-mode gdb-disassembly-mode gdb-frames-mode gdb-locals-mode gdb-memory-mode gdb-registers-mode gdb-threads-mode gist-list-mode git-commit-mode gnus-article-mode gnus-browse-mode gnus-group-mode gnus-server-mode gnus-summary-mode google-maps-static-mode ibuffer-mode jde-javadoc-checker-report-mode magit-cherry-mode magit-diff-mode magit-log-mode magit-log-select-mode magit-popup-mode magit-popup-sequence-mode magit-process-mode magit-reflog-mode magit-refs-mode magit-revision-mode magit-stash-mode magit-stashes-mode magit-status-mode magit-mode magit-branch-manager-mode magit-commit-mode magit-key-mode magit-rebase-mode magit-wazzup-mode mh-folder-mode monky-mode mu4e-main-mode mu4e-headers-mode mu4e-view-mode notmuch-hello-mode notmuch-search-mode notmuch-show-mode occur-mode org-agenda-mode package-menu-mode proced-mode rcirc-mode rebase-mode recentf-dialog-mode reftex-select-bib-mode reftex-select-label-mode reftex-toc-mode sldb-mode slime-inspector-mode slime-thread-control-mode slime-xref-mode sr-buttons-mode sr-mode sr-tree-mode sr-virtual-mode tar-mode tetris-mode tla-annotate-mode tla-archive-list-mode tla-bconfig-mode tla-bookmarks-mode tla-branch-list-mode tla-browse-mode tla-category-list-mode tla-changelog-mode tla-follow-symlinks-mode tla-inventory-file-mode tla-inventory-mode tla-lint-mode tla-logs-mode tla-revision-list-mode tla-revlog-mode tla-tree-lint-mode tla-version-list-mode twittering-mode urlview-mode vc-annotate-mode vc-dir-mode vc-git-log-view-mode vc-hg-log-view-mode vc-svn-log-view-mode vm-mode vm-summary-mode w3m-mode wab-compilation-mode xgit-annotate-mode xgit-changelog-mode xgit-diff-mode xgit-revlog-mode xhg-annotate-mode xhg-log-mode xhg-mode xhg-mq-mode xhg-mq-sub-mode xhg-status-extra-mode term-mode eshell-mode dired-mode cider-browse-ns-mode cider-connections-buffer-mode cider-docview-mode cider-inspector-mode cider-popup-buffer-mode cider-repl-mode cider-stacktrace-mode cider-test-report-mode org-mode)))
+ '(evil-insert-state-modes
+   (quote
+    (erc-mode geiser-repl-mode gud-mode inferior-apl-mode inferior-caml-mode inferior-emacs-lisp-mode inferior-j-mode inferior-python-mode inferior-scheme-mode inferior-sml-mode internal-ange-ftp-mode prolog-inferior-mode reb-mode slime-repl-mode wdired-mode)))
+ '(evil-jumper-mode t)
+ '(evil-leader/leader "<SPC>")
+ '(evil-mode t)
+ '(evil-search-wrap nil)
+ '(evil-symbol-word-search t)
+ '(helm-buffers-fuzzy-matching t)
+ '(helm-command-prefix-key "C-c h")
+ '(helm-ff-file-name-history-use-recentf t)
+ '(helm-ff-search-library-in-sexp t)
+ '(helm-mode t)
+ '(helm-mode-handle-completion-in-region nil)
+ '(helm-move-to-line-cycle-in-source t)
+ '(helm-net-prefer-curl t)
+ '(helm-split-window-in-side-p t)
+ '(help-window-select t)
+ '(indent-tabs-mode nil)
+ '(inhibit-startup-screen t)
+ '(make-backup-files nil)
+ '(menu-bar-mode nil)
+ '(multi-term-dedicated-select-after-open-p t)
+ '(multi-term-program "/bin/bash")
+ '(org-use-speed-commands t)
+ '(projectile-completion-system (quote helm))
+ '(projectile-global-mode t)
+ '(projectile-switch-project-action (quote helm-projectile-find-file))
+ '(recentf-auto-cleanup (quote never))
+ '(savehist-mode t)
+ '(scroll-bar-mode nil)
+ '(term-bind-key-alist (quote (("C-c C-c" . term-interrupt-subjob))))
+ '(term-unbind-key-list (quote ("C-x" "C-c" "C-h" "C-y" "M-x" "M-:")))
+ '(text-mode-hook (quote (text-mode-hook-identify auto-fill-mode)))
+ '(tool-bar-mode nil)
+ '(venv-location "~/Prog/venv/")
+ '(winner-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#2e3434" :foreground "#eeeeec" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 128 :width normal :foundry "unknown" :family "Ubuntu Mono"))))
+ '(term-color-blue ((t (:background "cornflower blue" :foreground "cornflower blue")))))
---
+++ init.el
@@ -0,0 +1,119 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
+(let ((packages '(ace-jump-mode
+                  anaconda-mode
+                  cider
+                  cmake-mode
+                  company
+                  company-anaconda
+                  company-quickhelp
+                  evil
+                  evil-jumper
+                  evil-leader
+                  evil-nerd-commenter
+                  evil-paredit
+                  fill-column-indicator
+                  ggtags
+                  haskell-mode
+                  helm
+                  helm-projectile
+                  magit
+                  multi-term
+                  ox-gfm
+                  paredit
+                  projectile
+                  tangotango-theme
+                  virtualenvwrapper))
+      (need-refresh t))
+  (dolist (package packages)
+    (if (not (package-installed-p package))
+        (progn
+          (if need-refresh
+	      (progn
+		(package-refresh-contents)
+		(setq need-refresh nil)))
+          (package-install package)))))
+
+;; load custom settings
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
+;; winner
+(global-set-key (kbd "C-x w u") 'winner-undo)
+(global-set-key (kbd "C-x w r") 'winner-redo)
+
+;; windmove
+(windmove-default-keybindings)
+(global-set-key (kbd "C-x w h") 'windmove-left)
+(global-set-key (kbd "C-x w j") 'windmove-down)
+(global-set-key (kbd "C-x w k") 'windmove-up)
+(global-set-key (kbd "C-x w l") 'windmove-right)
+
+;; evil-leader
+(global-evil-leader-mode t)
+(evil-leader/set-key "j" 'evil-ace-jump-word-mode)
+(evil-leader/set-key "k" 'evil-ace-jump-char-mode)
+(evil-leader/set-key "b" 'helm-mini)
+(evil-leader/set-key "f" 'helm-find-files)
+
+;; evil
+(define-key evil-normal-state-map (kbd "C-]") (kbd "\\ M-."))
+
+;; helm
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-x b") 'helm-mini)
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action)
+(define-key helm-map (kbd "C-z") 'helm-select-action)
+(require 'helm-config)
+(define-key helm-command-map (kbd "g") 'helm-do-grep)
+(define-key helm-command-map (kbd "SPC") 'helm-all-mark-rings)
+
+;; helm-projectile
+(helm-projectile-on)
+
+;; after-init hooks
+(add-hook 'after-init-hook 'global-company-mode)
+
+;; prog-mode
+(add-hook 'prog-mode-hook 'linum-mode)
+(add-hook 'prog-mode-hook 'ggtags-mode)
+
+;; python-mode
+(add-hook 'python-mode-hook 'anaconda-mode)
+(add-hook 'python-mode-hook 'eldoc-mode)
+
+;; haskell-mode
+(add-hook 'haskell-mode-hook 'subword-mode) 
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+
+;; emacs-lisp-mode
+(add-hook 'emacs-lisp-mode-hook 'paredit-mode)
+(add-hook 'emacs-lisp-mode-hook 'evil-paredit-mode)
+(add-hook 'emacs-lisp-mode-hook (lambda ()
+                                  (eldoc-mode t)
+                                  (make-local-variable 'eldoc-documentation-function)
+                                  (setq-local eldoc-documentation-function nil)))
+
+;; multi-term
+(global-set-key (kbd "C-x t t") 'multi-term-dedicated-toggle)
+(global-set-key (kbd "C-x t c") 'multi-term-dedicated-close)
+(global-set-key (kbd "C-x t s") 'multi-term-dedicated-select)
+(global-set-key (kbd "C-x t n") 'multi-term-next)
+(global-set-key (kbd "C-x t p") 'multi-term-prev)
+
+;; magit
+(global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
+
+;; evil-nerd-commenter
+(evilnc-default-hotkeys)
+(define-key evil-visual-state-map (kbd ",ci") 'evilnc-comment-or-uncomment-lines)
+
+;; virtualenvwrapper
+(venv-initialize-eshell)
