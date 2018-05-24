e5ac98cf444239d1c1cce110e3735baeb09316da
montag451 <montag451@laposte.net>
2018-05-24 22:02:34 +0200

    Protect against missing functions when Emacs is compiled without window system support

--- init.el
+++ init.el
@@ -68,8 +68,10 @@ value of the symbol."
 
 ;; save some screen estate
 (menu-bar-mode -1)
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
+(when (fboundp 'scroll-bar-mode)
+  (scroll-bar-mode -1))
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
 
 ;; set font
 (when window-system
